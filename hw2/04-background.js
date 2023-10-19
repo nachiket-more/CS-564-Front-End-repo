4393707ee2bd2a2b7d47a727e5bf5e6c4507d05d
Nachiket <nmore@pdx.edu>
2023-10-18 18:12:26 -0700

    homework2

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,41 @@
 // Add your code here
+
+document.addEventListener("DOMContentLoaded", function () {
+  const body = document.getElementsByTagName("body")[0];
+
+  const startStopBtn = document.getElementById("start-stop-btn");
+  const timeInput = document.getElementById("time-input");
+
+  const startColor = "rgb(11, 94, 215)";
+  const stopColor = "rgb(187, 45, 59)";
+  var timerValue = 3000;
+
+  startStopBtn.addEventListener("click", function () {
+    if (timeInput.value !== "") {
+      if (startStopBtn.style.backgroundColor === startColor) {
+        startStopBtn.style.backgroundColor = stopColor;
+        startStopBtn.innerHTML = "Stop";
+        timerValue = timeInput.value;
+      } else {
+        startStopBtn.style.backgroundColor = startColor;
+        startStopBtn.innerHTML = "Start";
+        timerValue = 3000;
+      }
+    }
+  });
+
+  function getRandomRGBA() {
+    const r = Math.floor(Math.random() * 256); // Red component (0-255)
+    const g = Math.floor(Math.random() * 256); // Green component (0-255)
+    const b = Math.floor(Math.random() * 256); // Blue component (0-255)
+
+    return `rgba(${r},${g},${b},${0.7})`;
+  }
+
+  function timerColor() {
+    const randomColor = getRandomRGBA();
+    body.style.background = randomColor;
+  }
+
+  setInterval(timerColor, timerValue);
+});
--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,37 @@
 // Add your code here
+const form = document.getElementsByClassName("form")[0];
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const fullNameVale = form.elements.fullname.value;
+  const emailValue = form.elements.email.value;
+  const registrationValue = form.elements.registration.value;
+  const checkboxInputs = form.elements.checkbox;
+  const textValue = form.elements.textArea.value;
+
+  console.group("========= Form submission =========");
+  console.log(
+    "Full Name: ",
+    fullNameVale == "" ? "no submission" : fullNameVale
+  );
+  console.log("Email: ", emailValue == "" ? "no submission" : emailValue);
+  console.log(
+    "Registration Status: ",
+    registrationValue == "" ? "no submission" : registrationValue
+  );
+
+  const checkedCourse = []
+  checkboxInputs.forEach((checkbox) => {
+    if (checkbox.checked) {
+      checkedCourse.push(checkbox.value)
+    }
+  });
+  console.log("Have you taken any of the following courses?: ", checkedCourse.length==0 ? "no submission" : checkedCourse)
+
+  console.log(
+    "Anything else?: ",
+    textValue == "" ? "no submission" : textValue
+  );
+  console.groupEnd();
+});
