b3afda6e2f1020bdc7c1f2951393d23893323bb9
Nachiket <nmore@pdx.edu>
2023-11-05 11:17:56 -0800

    homework3 push fixed

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,4 +1,42 @@
 // url for the Thrones API
-const url = 'https://thronesapi.com/api/v2/Characters';
+const url = "https://thronesapi.com/api/v2/Characters";
 
-fetch(url);
+async function getData() {
+  const response = await fetch(url);
+  const characters = await response.json();
+  console.log(characters);
+  addData(characters);
+}
+
+function addData(data) {
+  const containerDiv = document.getElementById("character-container");
+
+  data.forEach((item) => {
+    const itemDiv = document.createElement("div");
+    itemDiv.classList.add("col");
+
+    const imgDiv = document.createElement("img");
+    imgDiv.classList.add("img");
+    imgDiv.src = item.imageUrl;
+    imgDiv.alt = "";
+
+    const fullNameDiv = document.createElement("div");
+    fullNameDiv.textContent = item.fullName;
+    fullNameDiv.classList.add("name");
+
+    const titleDiv = document.createElement("div");
+    titleDiv.textContent = item.title;
+    titleDiv.classList.add("title");
+
+    itemDiv.appendChild(imgDiv);
+    itemDiv.appendChild(fullNameDiv);
+    itemDiv.appendChild(titleDiv);
+
+    containerDiv.appendChild(itemDiv);
+    
+  });
+
+  document.body.appendChild(containerDiv);
+}
+
+getData();
--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -1,50 +1,88 @@
-const backgroundColors = [
-  'rgba(54, 162, 235, 0.8)',
-  'rgba(255, 206, 86, 0.8)',
-  'rgba(255, 99, 132, 0.8)',
-  'rgba(75, 192, 192, 0.8)',
-  'rgba(153, 102, 255, 0.8)',
-  'rgba(255, 159, 64, 0.8)',
-  'rgba(199, 199, 199, 0.8)',
-  'rgba(83, 102, 255, 0.8)',
-  'rgba(40, 159, 64, 0.8)',
-  'rgba(210, 199, 199, 0.8)',
-  'rgba(78, 52, 199, 0.8)',
-];
-
-const borderColors = [
-  'rgba(54, 162, 235, 1)',
-  'rgba(255, 206, 86, 1)',
-  'rgba(255, 99, 132, 1)',
-  'rgba(75, 192, 192, 1)',
-  'rgba(153, 102, 255, 1)',
-  'rgba(255, 159, 64, 1)',
-  'rgba(159, 159, 159, 1)',
-  'rgba(83, 102, 255, 1)',
-  'rgba(40, 159, 64, 1)',
-  'rgba(210, 199, 199, 1)',
-  'rgba(78, 52, 199, 1)',
-];
+const backgroundColors = [];
+const borderColors = [];
 
 // url for the Thrones API
-const url = 'https://thronesapi.com/api/v2/Characters';
+const url = "https://thronesapi.com/api/v2/Characters";
 
-const renderChart = () => {
-  const donutChart = document.querySelector('.donut-chart');
+var family = [];
+
+async function getData() {
+  const response = await fetch(url);
+  const characters = await response.json();
+  characters.forEach((item) => {
+    console.log(item)
+    familyName = updateFamily(item.family);
+    family.push(familyName);
+  });
+  sortHouses(family);
+}
+
+getData();
+
+const updateFamily = (family) => {
+  family = family.replace(/House/, "").trim();
+  const cleanHouseNames = {
+    Lanister: "Lannister",
+    None: "Unknown",
+    Unkown: "Unknown",
+    Targaryan: "Targaryen",
+    Targaryn: "Targaryen",
+    "": "Unknown",
+  };
+  return cleanHouseNames[family] || family;
+};
+
+const sortHouses = (family) => {
+  var houses = Array.from(new Set(family)).map((a) => ({
+    house: a,
+    count: family.filter((f) => f === a).length,
+  }));
+
+  const houseArray = houses.map((item) => item.house);
+  const countArray = houses.map((item) => item.count);
+
+  addColors(houseArray);
+
+  renderChart(houseArray, countArray);
+};
+
+function getRandomRGBA() {
+  const r = Math.floor(Math.random() * 256); // Red component (0-255)
+  const g = Math.floor(Math.random() * 256); // Green component (0-255)
+  const b = Math.floor(Math.random() * 256); // Blue component (0-255)
+
+  backgroundColors.push(`rgb(${r},${g},${b}, ${0.8})`);
+  borderColors.push(`rgb(${r},${g},${b}, ${1})`);
+}
+
+function addColors(houseArray) {
+  houseArray.forEach((house) => {
+    getRandomRGBA();
+  });
+  
+}
+
+const renderChart = (houseArray, countArray) => {
+  const donutChart = document.querySelector(".donut-chart");
 
   new Chart(donutChart, {
-    type: 'doughnut',
+    type: "doughnut",
     data: {
-      labels: ['label', 'label', 'label', 'label'],
+      labels: houseArray,
       datasets: [
         {
-          label: 'My First Dataset',
-          data: [1, 12, 33, 5],
+          label: "My First Dataset",
+          data: countArray,
           backgroundColor: backgroundColors,
           borderColor: borderColors,
           borderWidth: 1,
         },
       ],
+    },
+    options: {
+      legend: {
+        display: false, 
+      },
     },
   });
 };
